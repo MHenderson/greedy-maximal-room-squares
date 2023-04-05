f935f881c181562180b0f3bfa3e5d98098325590
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-04-05 07:43:28 +0000

    important but minor changes

--- R/roomr62.R
+++ R/roomr62.R
@@ -3,18 +3,19 @@ library(R6)
 Room <- R6Class(
   classname = "Room",
   public = list(
-       size = NULL,
-      cells = NULL,
-    symbols = NULL,
-    free_pairs = NULL,
+
+           size = NULL,
+          cells = NULL,
+        symbols = NULL,
+     free_pairs = NULL,
     empty_cells = NULL,
     
     initialize = function(size = NA) {
       self$size <- size
-      self$symbols <- 0:(size - 1)
+      self$symbols <- 0:(self$size - 1)
       self$cells <- expand_grid(row = 1:(self$size - 1), col = 1:(self$size - 1)) %>%
         mutate(first = as.integer(NA), second = as.integer(NA)) %>%
-        mutate(avail = list(0:(n - 1)))
+        mutate(avail = list(0:(self$size - 1)))
       self$free_pairs <- all_pairs(self$size)
       self$empty_cells <- all_ordered_pairs(self$size - 1)
     },
@@ -31,16 +32,20 @@ Room <- R6Class(
     is_available = function(e, p) {
       p[1] %in% self$cells[self$cells$row == e[1] & self$cells$col == e[2], "avail"]$avail[[1]] && p[2] %in% self$cells[self$cells$row == e[1] & self$cells$col == e[2], "avail"]$avail[[1]]
     }
+
   ),
   active = list(
+    
     n_filled = function() {
       self$cells %>%
         filter(!is.na(first)) %>% 
         nrow()
     },
+    
     volume = function() {
       round(self$n_filled/choose(max(self$cells$col) + 1, 2), 6)
     }
+
   )
 )
 
