30782d63ef62536bf20a1469e2d3710d863b4458
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-07-23 15:38:35 +0100

    Use wallis (#6)
    * Remove un-used functions.
    
    * Simplify the README.
    
    * Re-init renv.
    
    * Don't return cells from random_room.
    
    * Simplify the targets pipeline.
    
    * Ignore .Rhistory.
    
    * Remove the Meszka-Rosa table data set.
    
    * Remove the random_room function.
    
    * Add greedy1 and greedy2 functions.
    
    * Rewrite the pipeline.
    
    * Add a table image to the README.
    
    * renv snapshot.

--- _targets.R
+++ _targets.R
@@ -1,57 +1,49 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
-
-# Load packages required to define the pipeline:
 library(targets)
-library(tarchetypes) # Load other packages as needed. # nolint
 
-# Set target options:
 tar_option_set(
-  packages = c("dplyr", "here", "purrr", "readr", "tibble"), # packages that your targets need to run
-  format = "rds" # default storage format
-  # Set other options as needed.
+  packages = c("dplyr", "flextable", "here", "purrr", "readr", "tibble", "tidyr", "wallis"),
+  format = "rds"
 )
 
-# tar_make_clustermq() configuration (okay to leave alone):
-options(clustermq.scheduler = "multicore")
-
-# tar_make_future() configuration (okay to leave alone):
-# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
-
-# Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed. # nolint
 
 list(
-  tar_target(orders, seq(10, 50, 2)),
-  tar_target(seeds, 42:44),
+  tar_target(orders, seq(2, 22, 2)),
   tar_target(
     results,
-    random_room(orders, seeds),
-    pattern = cross(orders, seeds)
+    tibble(
+      n = as.integer(orders),
+      greedy1 = list(greedy1(orders)),
+      greedy2 = list(greedy2(orders))
+    ) |>
+      pivot_longer(-n) |>
+      mutate(
+        volume = map_dbl(value, "volume"),
+        n_filled = map_dbl(value, "n_filled"),
+        cells = map(value, "cells")
+      ) %>%
+      select(-value),
+    pattern = map(orders)
   ),
   tar_target(
-    name = meszka,
-    command = read_rds(here("data", "meszka-rosa-table.rds"))
+    final_results,
+    results |>
+      select(-cells) |>
+      pivot_wider(names_from = name, values_from = c(volume, n_filled)) |>
+      rename(
+        t1 = n_filled_greedy1,
+        t2 = n_filled_greedy2,
+        r1 = volume_greedy1,
+        r2 = volume_greedy2
+      ) |>
+      select(n, t1, t2, r1, r2)
   ),
   tar_target(
-    name = final_results,
-    command = {
-      results %>%
-        group_by(n) %>%
-        summarise(t3 = max(n_filled)) %>%
-        left_join(meszka) %>%
-        mutate(
-          `t3 - t1` = t3 - t1,
-                 r3 = t3 / nc2
-        ) %>%
-        select(n, nc2, t1, t2, t3, r1, r2, r3)
-    }
+    name = final_results_as_flextable,
+    command = flextable(final_results) |> theme_vanilla()
   ),
-  tar_render(
-    name = readme,
-    "README.Rmd"
+  tar_target(
+    name = save_as_png,
+    command = save_as_image(final_results_as_flextable, path = "png/final-results.png")
   )
 )
