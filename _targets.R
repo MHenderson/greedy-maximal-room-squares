cbe9881dd7c5043304aa8077505c30c6b65c5a00
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-07-23 14:33:30 +0000

    Rewrite the pipeline.

--- _targets.R
+++ _targets.R
@@ -1,27 +1,49 @@
 library(targets)
 
 tar_option_set(
-  packages = c("dplyr", "here", "purrr", "readr", "tibble", "wallis"),
+  packages = c("dplyr", "flextable", "here", "purrr", "readr", "tibble", "tidyr", "wallis"),
   format = "rds"
 )
 
 tar_source()
 
 list(
-  tar_target(orders, seq(10, 22, 2)),
-  tar_target(seeds, 42:48),
+  tar_target(orders, seq(2, 22, 2)),
   tar_target(
     results,
-    command = random_room(orders, seeds),
-    pattern = cross(orders, seeds)
+    tibble(
+      n = as.integer(orders),
+      greedy1 = list(greedy1(orders)),
+      greedy2 = list(greedy2(orders))
+    ) |>
+      pivot_longer(-n) |>
+      mutate(
+        volume = map_dbl(value, "volume"),
+        n_filled = map_dbl(value, "n_filled"),
+        cells = map(value, "cells")
+      ) %>%
+      select(-value),
+    pattern = map(orders)
   ),
   tar_target(
-    name = results_summary,
-    command = results |>
-      group_by(n) |>
-      summarise(
-        min_filled = min(n_filled),
-        max_filled = max(n_filled)
-      )
+    final_results,
+    results |>
+      select(-cells) |>
+      pivot_wider(names_from = name, values_from = c(volume, n_filled)) |>
+      rename(
+        t1 = n_filled_greedy1,
+        t2 = n_filled_greedy2,
+        r1 = volume_greedy1,
+        r2 = volume_greedy2
+      ) |>
+      select(n, t1, t2, r1, r2)
+  ),
+  tar_target(
+    name = final_results_as_flextable,
+    command = flextable(final_results) |> theme_vanilla()
+  ),
+  tar_target(
+    name = save_as_png,
+    command = save_as_image(final_results_as_flextable, path = "png/final-results.png")
   )
 )
